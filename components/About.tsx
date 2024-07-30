0821fa6825e3bd3101a57f492fb62097e00270ec
Bobby <blipps@330cloud.com>
2024-07-29 22:48:13 -0400

    Updating more content

--- components/About.tsx
+++ components/About.tsx
@@ -9,12 +9,12 @@ export default function About() {
                     <AboutCard
                         title="Front-End Development"
                         icon="/icons/design.svg"
-                        description="I love building web front-ends. Taking a design and turning it into a fully functional website is a rewarding experience. I have experience with many front-end technologies including React, Vue, and Bootstrap. Just to name a few."
+                        description="I love building web front-ends. Taking a design and turning it into a fully functional website is a rewarding journey. I have experience with many front-end technologies including React, Vue, and Bootstrap. Just to name a few."
                         projects={7} />
                     <AboutCard
                         title="Cloud Development"
                         icon="/icons/code.svg"
-                        description="I love tinkering with cloud services. I have experience with AWS and love serverless technologies. Especially Lambda, DynamoDB, and S3. I also enjoy working with CI/CD pipelines and infrastructure as code using the AWS CDK. I also see endless posibilities with AI in the cloud, especially with AWS Bedrock."
+                        description="I love tinkering with cloud services. I am an AWS Certified Developer and love serverless technologies. Especially Lambda, DynamoDB, and S3. I enjoy working with CI/CD pipelines and infrastructure as code using the AWS CDK. I also see endless posibilities with AI in the cloud, especially with AWS Bedrock."
                         projects={10} />
                     <AboutCard
                         title="Mobile"
@@ -26,9 +26,9 @@ export default function About() {
                 <div className="flex-full about-text">
                     <h5 className="gray">Introduction</h5>
                     <h1 className="white">Hello, I&#39;m Bobby Lipps.</h1>
-                    <h3 className="white">Design is not just what it looks like. Design is how it works!</h3>
-                    <p className="gray">I have proceeded my dream to be a developer as it has been my lifelong ambition. I am a talented Front-End developer with a UI/UX design background. During my 4 years of work as a freelancer, I had the opportunity to enhance my expertise by collaborating with different companies and by creating useful content for both business and customer use.<br/><br/>
-                    I am naturally persevered, self-confident, quietly curios, innovative and constantly challenging my skills.</p>
+                    <h3 className="white">Crafting the Future Through Code: A Journey Beyond the Screen</h3>
+                    <p className="gray">My professional path is driven by a deep-seated passion for exploring the vast possibilities within cloud and frontend development. This enthusiasm is not just about building applications but about constructing gateways that transform user interaction and operational efficiency. Programming for me is akin to diving into a rabbit hole where each challenge is an opportunity to innovate and each line of code is a step towards redefining functionality.<br/><br/>
+                    In this ever-evolving digital landscape, I thrive on the thrill of continuous learning and the satisfaction of overcoming complex problems. Whether it's mastering AWS services or developing seamless front-end interfaces, I am perpetually in pursuit of creating solutions that are not only effective but also transformative. My commitment to technology is fueled by a curiosity that keeps me at the cutting edge, constantly challenging my skills to craft experiences that make a difference.</p>
                 </div>
             </div>
 
