1e93e77bec9401c3fb9afc79b645e837ae20c0bc
Bobby <blipps@330cloud.com>
2024-07-31 11:46:51 -0400

    Content fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,10 +11,10 @@ export default function Header() {
                 <div>
                     <button>Front-End / Cloud Developer</button>
                     <h1 className="white">Building Tomorrow&#39;s Technology,</h1>
-                    <h1 className="white">One Line of Code at a Time</h1>
+                    <h1 className="white">One Line of Code at a Time.</h1>
                     <p className="gray">I build websites and cloud applications.</p>
                     <a className="green" href="mailto:bllipps199@gmail.com">Let&#39;s chat!</a>
-                </div> 
+                </div>
                 <Image src="/images/bobby.png" width={463} height={513} alt="Bobby Lipps"  /> 
             </div>
         </header>
